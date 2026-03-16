0cd33561c315813d575a88a3f30330d07520ae9c
princeverma <princeverma2072644@gmail.com>
2026-03-16 14:41:40 +0530

    added

--- server.js
+++ server.js
@@ -203,7 +203,8 @@ session_id:this.lastID
 // -----------------------------
 // SERVER START
 // -----------------------------
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000")
-})
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
